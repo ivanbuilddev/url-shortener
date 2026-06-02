a1dbd8caf55f77c461aa0a88314663d28ad5f10c
ivanbuilddev <ivanbuilddev@gmail.com>
2026-06-02 11:42:36 +0200

    feat: Migrations applied

---
+++ UrlShortener/Migrations/20260602094204_ImplementedUser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ShortUrls",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "ShortUrls",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "ShortUrls",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_UserId",
+                table: "ShortUrls",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShortUrls_User_UserId",
+                table: "ShortUrls",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShortUrls_User_UserId",
+                table: "ShortUrls");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShortUrls_UserId",
+                table: "ShortUrls");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ShortUrls");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "ShortUrls");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ShortUrls");
+        }
+    }
+}
